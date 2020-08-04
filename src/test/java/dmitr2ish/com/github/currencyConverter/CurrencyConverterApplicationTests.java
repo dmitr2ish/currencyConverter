3d8f30688e18147f55a9b0feaf12003ee8614fbe
dmitr2ish <dmitr2ish@yandex.ru>
2020-08-04 10:15:55 +0500

    Initial commit

---
+++ src/test/java/dmitr2ish/com/github/currencyConverter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package dmitr2ish.com.github.currencyConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
