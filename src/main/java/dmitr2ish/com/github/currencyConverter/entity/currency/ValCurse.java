0fb5cd4d118a915c89edfc7505d5360384d24057
dmitr2ish <dmitr2ish@yandex.ru>
2020-08-06 06:23:53 +0500

    - ValCurse entity

---
+++ src/main/java/dmitr2ish/com/github/currencyConverter/entity/currency/ValCurse.java
@@ -0,0 +1,48 @@
+package dmitr2ish.com.github.currencyConverter.entity.currency;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "cc_curse")
+public class ValCurse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Date date;
+
+    public ValCurse(String name, Date date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public ValCurse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
