cbac92bb3b2dac1ecdbe561e378037dcf53d2460
dmitr2ish <dmitr2ish@yandex.ru>
2020-08-06 06:19:57 +0500

    - added Currency entity

---
+++ src/main/java/dmitr2ish/com/github/currencyConverter/entity/currency/Currency.java
@@ -0,0 +1,99 @@
+package dmitr2ish.com.github.currencyConverter.entity.currency;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "cc_currency")
+public class Currency {
+
+    @Id
+    private String id;
+
+    private Long numCode;
+
+    private String charCode;
+
+    private Integer nominal;
+
+    private String name;
+
+    private BigDecimal value;
+
+    @OneToMany
+    private List<ValCurse> curse;
+
+    public Currency(String id, Long numCode, String charCode, Integer nominal, String name, BigDecimal value, List<ValCurse> curse) {
+        this.id = id;
+        this.numCode = numCode;
+        this.charCode = charCode;
+        this.nominal = nominal;
+        this.name = name;
+        this.value = value;
+        this.curse = curse;
+    }
+
+    public Currency() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Long getNumCode() {
+        return numCode;
+    }
+
+    public void setNumCode(Long numCode) {
+        this.numCode = numCode;
+    }
+
+    public String getCharCode() {
+        return charCode;
+    }
+
+    public void setCharCode(String charCode) {
+        this.charCode = charCode;
+    }
+
+    public Integer getNominal() {
+        return nominal;
+    }
+
+    public void setNominal(Integer nominal) {
+        this.nominal = nominal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public List<ValCurse> getCurse() {
+        return curse;
+    }
+
+    public void setCurse(List<ValCurse> curse) {
+        this.curse = curse;
+    }
+}
