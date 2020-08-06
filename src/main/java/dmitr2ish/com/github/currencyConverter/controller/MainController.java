0805742b27e23c50899ba1b98e90bf487da4aea5
dmitr2ish <dmitr2ish@yandex.ru>
2020-08-06 06:22:05 +0500

    - added main controller where implement back door init method and login page get methods

---
+++ src/main/java/dmitr2ish/com/github/currencyConverter/controller/MainController.java
@@ -0,0 +1,38 @@
+package dmitr2ish.com.github.currencyConverter.controller;
+
+import dmitr2ish.com.github.currencyConverter.entity.user.Role;
+import dmitr2ish.com.github.currencyConverter.entity.user.User;
+import dmitr2ish.com.github.currencyConverter.service.RoleService;
+import dmitr2ish.com.github.currencyConverter.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    final private UserService userService;
+    final private RoleService roleService;
+
+    @Autowired
+    public MainController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping(value = "/bdinit")
+    public String secretPage() {
+        roleService.add(new Role("ADMIN"));
+        roleService.add(new Role("USER"));
+        User user = new User();
+        user.setLogin("test@test.ru");
+        user.setPassword("test");
+        user.setRoles(roleService.getAll());
+        userService.addUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping(value = "/login")
+    public String loginPage() {
+        return "login";
+    }
+}
